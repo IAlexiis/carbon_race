855a2858803b9961f172959446ed3c422a72bda8
Alexis Marin <mail.alexismarin@gmail.com>
2025-11-25 18:45:32 +0100

    Added files, deleted files and created the main page style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup></script>
 
 <template>
-  <div>
-  <h1>Page d'accueil test</h1>
-  </div>
+<body>
+    <header>
+        <h1>Carbon Race</h1>
+    </header>
+    <section class="hero">
+        <p>A quick and accessible companion for your Carbon Race game.
+        Find the rules, answer keys, and play mini-games directly from your mobile.</p>
+    </section>
+    <div class="button-section">
+        <button>Rules</button>
+        <button>Answers</button>
+    </div>
+</body>
 </template>
+
 
 <style scoped></style>
